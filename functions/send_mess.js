54a2c49fd2c94d25e2f6aa5232b1926fcfe14bfa
qerq90 <alsavidi37@gmail.com>
2020-02-16 16:19:18 +0300

    bug

--- functions/send_mess.js
+++ functions/send_mess.js
@@ -4,7 +4,7 @@ const api = require("node-vk-bot-api/lib/api");
 module.exports = (event_info, user_ids) => {
     api("messages.send", {
         message: event_info,
-        user_ids: user_ids,
+        user_ids: user_ids.toString(),
         random_id: 0,
         access_token: process.env.TOKEN
     });
