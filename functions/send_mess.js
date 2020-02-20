aa4ed17ff89951d2d68f886861fcffa9734c8749
qerq90 <alsavidi37@gmail.com>
2020-02-21 01:58:40 +0300

    err catching

--- functions/send_mess.js
+++ functions/send_mess.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const api = require("node-vk-bot-api/lib/api");
 
 module.exports = (event_info, user_ids) => {
-    api("messages.send", {
-        message: event_info,
-        user_ids: user_ids.toString(),
-        random_id: 0,
-        access_token: process.env.TOKEN
-    });
+    try {
+        api("messages.send", {
+            message: event_info,
+            user_ids: user_ids.toString(),
+            random_id: 0,
+            access_token: process.env.TOKEN
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
