746f0c3655bc22d1c9dcf9e7a38336276864bcce
qerq90 <alsavidi37@gmail.com>
2020-02-25 18:41:31 +0300

    idiot

--- functions/send_mess.js
+++ functions/send_mess.js
@@ -3,9 +3,9 @@ const api = require("node-vk-bot-api/lib/api");
 const duel_check = require("./duel_check");
 
 module.exports = (event_info, user_ids) => {
-    if (!user_ids.length) return;
-
     user_ids = duel_check(user_ids, event_info);
+
+    if (!user_ids.length) return;
 
     try {
         api("messages.send", {
